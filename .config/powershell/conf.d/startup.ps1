ab998f7f2a145d83d48f710e90ecc1da222b2230
T4ko0522 <tako.renraku1@gmail.com>
2026-04-05 18:29:00 +0900

    perf: ⚡️ Terminal-Iconをバックグラウンドでロードするように

--- .config/powershell/conf.d/startup.ps1
+++ .config/powershell/conf.d/startup.ps1
@@ -1,5 +1,14 @@
-# モジュールのインポート
-Import-Module Terminal-Icons -ErrorAction SilentlyContinue
+# Terminal-Icons をアイドル時にバックグラウンドロード（プロンプト表示後に実行）
+$global:_lsGlyphs = $null
+$global:_lsTheme = $null
+Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -MaxTriggerCount 1 -Action {
+    Import-Module Terminal-Icons -ErrorAction SilentlyContinue
+    if (Get-Module Terminal-Icons -ErrorAction SilentlyContinue) {
+        $global:_lsGlyphs = @{}
+        Get-TerminalIconsGlyphs | ForEach-Object { $global:_lsGlyphs[$_.Name] = $_.Value }
+        $global:_lsTheme = Get-TerminalIconsTheme
+    }
+} | Out-Null
 
 # https://github.com/antfu-collective/ni とNew-Itemの競合を無効化
 Remove-Item Alias:ni -Force -ErrorAction Ignore
@@ -9,7 +18,15 @@ if (-not (Get-Process -Name yasb -ErrorAction SilentlyContinue)) {
     Start-Process yasb -WindowStyle Hidden
 }
 
-# Starship プロンプト初期化
+# Starship プロンプト初期化（キャッシュで高速化）
 if (Get-Command starship -ErrorAction SilentlyContinue) {
-    Invoke-Expression (& starship init powershell)
+    $starshipCache = Join-Path $env:LOCALAPPDATA "starship_init.ps1"
+    $starshipBin = (Get-Command starship).Source
+    if ((Test-Path $starshipCache) -and (Get-Item $starshipCache).LastWriteTime -gt (Get-Item $starshipBin).LastWriteTime) {
+        . $starshipCache
+    } else {
+        $initScript = & starship init powershell --print-full-init
+        $initScript | Set-Content -LiteralPath $starshipCache -Encoding UTF8
+        Invoke-Expression $initScript
+    }
 }
