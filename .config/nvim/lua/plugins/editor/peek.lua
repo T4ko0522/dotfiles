4fed83ed8178223b8f611ca10db90a32d314fb26
T4ko0522 <tako.renraku1@gmail.com>
2026-05-14 13:35:03 +0900

    feat(nvim): ✨ peek.nvim で Markdown プレビューを追加
    - Deno ベースの軽量プレビュー (build: deno task --quiet build:fast)
    - :PeekOpen / :PeekClose / :PeekToggle の 3 コマンドを定義
    - <leader>mp でトグル (markdown ファイルタイプ限定)
    - app="browser" を採用し既定ブラウザで開く (WebView2 依存を回避)

---
+++ .config/nvim/lua/plugins/editor/peek.lua
@@ -0,0 +1,31 @@
+return {
+  "toppair/peek.nvim",
+  build = "deno task --quiet build:fast",
+  cmd = { "PeekOpen", "PeekClose", "PeekToggle" },
+  keys = {
+    { "<leader>mp", "<cmd>PeekToggle<CR>", desc = "Markdown Preview (peek)", ft = "markdown" },
+  },
+  config = function()
+    local peek = require("peek")
+    peek.setup({
+      auto_load = true,
+      close_on_bdelete = true,
+      syntax = true,
+      theme = "dark",
+      update_on_change = true,
+      app = "browser", -- "webview" だと WebView2 依存。"browser" は既定ブラウザで開く
+      filetype = { "markdown" },
+      throttle_at = 200000,
+      throttle_time = "auto",
+    })
+    vim.api.nvim_create_user_command("PeekOpen", peek.open, {})
+    vim.api.nvim_create_user_command("PeekClose", peek.close, {})
+    vim.api.nvim_create_user_command("PeekToggle", function()
+      if peek.is_open() then
+        peek.close()
+      else
+        peek.open()
+      end
+    end, {})
+  end,
+}
