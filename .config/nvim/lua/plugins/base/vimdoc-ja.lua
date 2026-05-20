11dc125fb175058dd1a1f9f4c4d82ca12b349403
T4ko0522 <tako.renraku1@gmail.com>
2026-05-20 11:06:44 +0900

    style: 💄 フォントカラーを調整

--- .config/nvim/lua/plugins/base/vimdoc-ja.lua
+++ .config/nvim/lua/plugins/base/vimdoc-ja.lua
@@ -4,4 +4,9 @@ return {
   keys = {
     { "h", mode = "c" },
   },
+  -- :helptags が doc/tags-ja の `!_TAG_FILE_ENCODING` 行を剥がしてしまい
+  -- :Lazy update が "local changes" で落ちるため、git 側で変更を無視させる。
+  build = function(plugin)
+    vim.fn.system({ "git", "-C", plugin.dir, "update-index", "--skip-worktree", "doc/tags-ja" })
+  end,
 }
