16d84e138494c48996c1d5aaf293bb03b59305db
T4ko0522 <tako.renraku1@gmail.com>
2026-06-03 22:58:56 +0900

    chore: 🔧 vicinaeのやつcommit漏れ

--- nix-configs/modules/nix.nix
+++ nix-configs/modules/nix.nix
@@ -1,10 +1,14 @@
 {dotfilesDir, ...}: {
   nixpkgs.config.allowUnfree = true;
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    substituters = ["https://vicinae.cachix.org"];
+    trusted-public-keys = ["vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="];
+  };
 
   programs.nh = {
     enable = true;
