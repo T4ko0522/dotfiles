708bb35bc9325f6bf810e52f8569af7201759f9c
T4ko0522 <tako.renraku1@gmail.com>
2026-06-03 18:26:52 +0900

    feat: ✨ ゲーム関係のを追加

---
+++ nix-configs/home/packages/gaming.nix
@@ -0,0 +1,18 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    (prismlauncher.override {
+      jdks = [
+        jdk25
+        jdk21
+        jdk17
+        jdk8
+      ];
+    })
+    alcom
+    osu-lazer-bin
+    protonup-qt
+    vrcx
+    wineWow64Packages.stable
+    winetricks
+  ];
+}
