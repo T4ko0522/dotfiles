85d93271b3f06bf121f9edb689a9c43927e326b6
T4ko0522 <tako.renraku1@gmail.com>
2026-06-09 00:45:42 +0900

    refactor: ♻️ unfreeを拒否してunfreeを明示的に追加

--- flake.nix
+++ flake.nix
@@ -17,78 +17,83 @@
     vicinae.url = "github:vicinaehq/vicinae";
   };
 
-  outputs = {
-    nixpkgs,
-    home-manager,
-    vial-qmk,
-    vicinae,
-    ...
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    dotfilesDir = "/home/t4ko/dotfiles";
-    keyboardLayout = {
-      xkbLayout = "jp";
-      xkbModel = "jp106";
-      consoleKeyMap = "jp106";
-      fcitxLayout = "jp";
-    };
-
-    mkNixos = {
-      configuration,
-      homeConfiguration,
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      vial-qmk,
+      vicinae,
+      ...
     }:
-      nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit dotfilesDir keyboardLayout;
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      dotfilesDir = self.outPath;
+      keyboardLayout = {
+        xkbLayout = "jp";
+        xkbModel = "jp106";
+        consoleKeyMap = "jp106";
+        fcitxLayout = "jp";
+      };
+
+      mkNixos =
+        {
+          configuration,
+          homeConfiguration,
+        }:
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit dotfilesDir keyboardLayout;
+          };
+          modules = [
+            home-manager.nixosModules.home-manager
+            vicinae.nixosModules.default
+            configuration
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "hm-backup";
+              home-manager.extraSpecialArgs = {
+                inherit dotfilesDir keyboardLayout;
+              };
+              home-manager.sharedModules = [ vicinae.homeManagerModules.default ];
+              home-manager.users.t4ko = import homeConfiguration;
+            }
+          ];
         };
-        modules = [
-          home-manager.nixosModules.home-manager
-          vicinae.nixosModules.default
-          configuration
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "hm-backup";
-            home-manager.extraSpecialArgs = {
-              inherit dotfilesDir keyboardLayout;
-            };
-            home-manager.sharedModules = [vicinae.homeManagerModules.default];
-            home-manager.users.t4ko = import homeConfiguration;
-          }
-        ];
+
+      nixos = mkNixos {
+        configuration = ./nix-configs/configuration.nix;
+        homeConfiguration = ./nix-configs/home.nix;
+      };
+      nixosCi = mkNixos {
+        configuration = ./nix-configs/configuration-ci.nix;
+        homeConfiguration = ./nix-configs/home-ci.nix;
+      };
+    in
+    {
+      nixosConfigurations = {
+        nixos = nixos;
+        default = nixos;
+        nixos-ci = nixosCi;
       };
 
-    nixos = mkNixos {
-      configuration = ./nix-configs/configuration.nix;
-      homeConfiguration = ./nix-configs/home.nix;
-    };
-    nixosCi = mkNixos {
-      configuration = ./nix-configs/configuration-ci.nix;
-      homeConfiguration = ./nix-configs/home-ci.nix;
-    };
-  in {
-    nixosConfigurations = {
-      nixos = nixos;
-      default = nixos;
-      nixos-ci = nixosCi;
-    };
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            avrdude
+            dfu-util
+            gcc-arm-embedded
+            git
+            gnumake
+            qmk
+            unzip
+          ];
 
-    devShells.${system} = {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          avrdude
-          dfu-util
-          gcc-arm-embedded
-          git
-          gnumake
-          qmk
-          unzip
-        ];
-
-        VIAL_QMK_SRC = "${vial-qmk}";
+          VIAL_QMK_SRC = "${vial-qmk}";
+        };
       };
     };
-  };
 }
