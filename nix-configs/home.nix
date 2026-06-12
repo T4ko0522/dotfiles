59c39334d74726673103ddd9565d94ff4ac538e9
T4ko0522 <tako.renraku1@gmail.com>
2026-06-13 00:59:52 +0900

    feat: ✨ リフレッシュレートを明記

--- nix-configs/home.nix
+++ nix-configs/home.nix
@@ -17,6 +17,7 @@
 
   t4ko.niri.monitors = {
     "DP-1" = {
+      mode = "1920x1080@144.000";
       position = {
         x = 3840;
         y = -840;
@@ -24,12 +25,14 @@
       transform = "90";
     };
     "DP-2" = {
+      mode = "1920x1080@360.000";
       position = {
         x = 0;
         y = 0;
       };
     };
     "HDMI-A-1" = {
+      mode = "1920x1080@75.000";
       position = {
         x = 1920;
         y = 0;
