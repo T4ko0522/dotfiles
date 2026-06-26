73e0fd2e682c515fd0a8bfe35b1c31c9e935f239
T4ko0522 <tako.renraku1@gmail.com>
2026-06-27 00:30:39 +0900

    feat(nix): ✨ cf-edgeNix の binary cache (nix.t4ko.pet) を substituter に追加

--- nix-configs/modules/nix.nix
+++ nix-configs/modules/nix.nix
@@ -6,10 +6,12 @@ _: {
       "flakes"
     ];
     substituters = [
+      "https://nix.t4ko.pet"
       "https://vicinae.cachix.org"
       "https://cache.numtide.com"
     ];
     trusted-public-keys = [
+      "nix.t4ko.pet-1:0eRO18L1/5diWYWboKKPTejQGhGCHNITwELiUaX7Kps="
       "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
     ];
