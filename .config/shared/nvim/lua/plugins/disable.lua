42f6e0a9951df86c4bbddfea7511db430920c459
T4ko0522 <tako.renraku1@gmail.com>
2026-07-06 21:35:23 +0900

    refactor(nvim): ♻️ ファイルツリーを snacks explorer に統一
    neo-tree を無効化し独自設定ファイルを削除する。
    
    - disable.lua: neo-tree.nvim を enabled=false に
    - neo-tree.lua を削除

--- .config/shared/nvim/lua/plugins/disable.lua
+++ .config/shared/nvim/lua/plugins/disable.lua
@@ -1,3 +1,6 @@
 -- LazyVim 標準プラグインや extras を無効化したい場合にここへ追加する
 -- 例: { "akinsho/bufferline.nvim", enabled = false }
-return {}
+return {
+  -- ファイルツリーは snacks explorer に統一するため neo-tree は無効化
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+}
