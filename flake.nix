a5ee2e8fb1402662abd0fc09513ddd30785bd40c
T4ko0522 <tako.renraku1@gmail.com>
2026-07-08 01:47:10 +0900

    💄 nix: flake.nix をalejandraで整形
    CIのNix formattingチェックがflake.nixの未整形を検出して落ちていたため、alejandraで自動整形した。ロジック変更なし。

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Dotfiles managed NixOS configuration";
 
   nixConfig = {
-    extra-substituters = [ "https://cache.numtide.com" ];
-    extra-trusted-public-keys = [ "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" ];
+    extra-substituters = ["https://cache.numtide.com"];
+    extra-trusted-public-keys = ["niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="];
   };
 
   inputs = {
@@ -31,111 +31,107 @@
     # actrun.url = "github:mizchi/actrun";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-      vial-qmk,
-      vicinae,
-      llm-agents,
-      spotify-cli,
-      # actrun,
-      ...
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    vial-qmk,
+    vicinae,
+    llm-agents,
+    spotify-cli,
+    # actrun,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    dotfilesDir = self.outPath;
+    keyboardLayout = {
+      xkbLayout = "jp";
+      xkbModel = "jp106";
+      xkbOptions = "caps:none";
+      consoleKeyMap = "jp106";
+      fcitxLayout = "jp";
+    };
+
+    mkNixos = {
+      configuration,
+      homeConfiguration,
     }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      dotfilesDir = self.outPath;
-      keyboardLayout = {
-        xkbLayout = "jp";
-        xkbModel = "jp106";
-        xkbOptions = "caps:none";
-        consoleKeyMap = "jp106";
-        fcitxLayout = "jp";
-      };
-
-      mkNixos =
-        {
-          configuration,
-          homeConfiguration,
-        }:
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = {
-            inherit dotfilesDir keyboardLayout;
-          };
-          modules = [
-            home-manager.nixosModules.home-manager
-            vicinae.nixosModules.default
-            configuration
-            {
-              nixpkgs.overlays = [
-                llm-agents.overlays.default
-                spotify-cli.overlays.default
-                # actrun.inputs.moonbit-overlay.overlays.default
-                # actrun.overlays.default
-                # (final: prev: {
-                #   actrun = prev.actrun.overrideAttrs (old: {
-                #     nativeBuildInputs = (old.nativeBuildInputs or []) ++ [final.makeWrapper];
-                #     postFixup =
-                #       (old.postFixup or "")
-                #       + ''
-                #         for bin in "$out"/bin/*; do
-                #           wrapProgram "$bin" --prefix LD_LIBRARY_PATH : "${final.openssl.out}/lib"
-                #         done
-                #       '';
-                #   });
-                # })
-              ];
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                backupFileExtension = "hm-backup";
-                extraSpecialArgs = {
-                  inherit dotfilesDir keyboardLayout;
-                };
-                sharedModules = [ vicinae.homeManagerModules.default ];
-                users.t4ko = import homeConfiguration;
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {
+          inherit dotfilesDir keyboardLayout;
+        };
+        modules = [
+          home-manager.nixosModules.home-manager
+          vicinae.nixosModules.default
+          configuration
+          {
+            nixpkgs.overlays = [
+              llm-agents.overlays.default
+              spotify-cli.overlays.default
+              # actrun.inputs.moonbit-overlay.overlays.default
+              # actrun.overlays.default
+              # (final: prev: {
+              #   actrun = prev.actrun.overrideAttrs (old: {
+              #     nativeBuildInputs = (old.nativeBuildInputs or []) ++ [final.makeWrapper];
+              #     postFixup =
+              #       (old.postFixup or "")
+              #       + ''
+              #         for bin in "$out"/bin/*; do
+              #           wrapProgram "$bin" --prefix LD_LIBRARY_PATH : "${final.openssl.out}/lib"
+              #         done
+              #       '';
+              #   });
+              # })
+            ];
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              backupFileExtension = "hm-backup";
+              extraSpecialArgs = {
+                inherit dotfilesDir keyboardLayout;
               };
-            }
-          ];
-        };
-
-      laptop = mkNixos {
-        configuration = ./nix-configs/hosts/laptop;
-        homeConfiguration = ./nix-configs/home.nix;
-      };
-      desktop = mkNixos {
-        configuration = ./nix-configs/hosts/desktop;
-        homeConfiguration = ./nix-configs/home.nix;
-      };
-      nixosCi = mkNixos {
-        configuration = ./nix-configs/configuration-ci.nix;
-        homeConfiguration = ./nix-configs/home-ci.nix;
-      };
-    in
-    {
-      nixosConfigurations = {
-        inherit laptop desktop;
-        default = laptop;
-        nixos-ci = nixosCi;
+              sharedModules = [vicinae.homeManagerModules.default];
+              users.t4ko = import homeConfiguration;
+            };
+          }
+        ];
       };
 
-      devShells.${system} = {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            avrdude
-            dfu-util
-            gcc-arm-embedded
-            git
-            gnumake
-            qmk
-            unzip
-          ];
+    laptop = mkNixos {
+      configuration = ./nix-configs/hosts/laptop;
+      homeConfiguration = ./nix-configs/home.nix;
+    };
+    desktop = mkNixos {
+      configuration = ./nix-configs/hosts/desktop;
+      homeConfiguration = ./nix-configs/home.nix;
+    };
+    nixosCi = mkNixos {
+      configuration = ./nix-configs/configuration-ci.nix;
+      homeConfiguration = ./nix-configs/home-ci.nix;
+    };
+  in {
+    nixosConfigurations = {
+      inherit laptop desktop;
+      default = laptop;
+      nixos-ci = nixosCi;
+    };
 
-          VIAL_QMK_SRC = "${vial-qmk}";
-        };
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          avrdude
+          dfu-util
+          gcc-arm-embedded
+          git
+          gnumake
+          qmk
+          unzip
+        ];
+
+        VIAL_QMK_SRC = "${vial-qmk}";
       };
     };
+  };
 }
