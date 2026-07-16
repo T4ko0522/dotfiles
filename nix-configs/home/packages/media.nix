05e16be55805c6d4e90fa29699206a4850d14d27
T4ko0522 <tako.renraku1@gmail.com>
2026-07-16 15:08:21 +0900

    ✨ media: AudacityとOBS Studioを追加

--- nix-configs/home/packages/media.nix
+++ nix-configs/home/packages/media.nix
@@ -7,12 +7,14 @@
   librepods = pkgs.callPackage ../../pkgs/librepods/package.nix {};
 in {
   home.packages = with pkgs; [
+    audacity
     ffmpeg
     gimp
     imv
     librepods
     kooha
     mpv
+    obs-studio
     yt-dlp
   ];
 
