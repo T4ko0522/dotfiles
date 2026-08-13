8dbbce8c1e9a809f5af5f40c02302b5eb3877061
T4ko0522 <tako.renraku1@gmail.com>
2026-08-13 17:10:14 +0900

    ✨ nh: 過去5世代のみ残しそれ以外を削除する

--- nix-configs/feature/modules/core/nh.nix
+++ nix-configs/feature/modules/core/nh.nix
@@ -1,6 +1,11 @@
-{dotfilesPath, ...}: {
+{ dotfilesPath, ... }: {
   programs.nh = {
     enable = true;
     flake = dotfilesPath;
+    clean = {
+      enable = true;
+      dates = "daily";
+      extraArgs = "--keep 5";
+    };
   };
 }
