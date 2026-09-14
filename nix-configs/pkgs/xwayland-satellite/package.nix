76171b6e3e76526e0dd778104357c5511fc9c210
T4ko0522 <tako.renraku1@gmail.com>
2026-09-14 14:31:17 +0900

    🔧 hashの整合性をupdate

--- nix-configs/pkgs/xwayland-satellite/package.nix
+++ nix-configs/pkgs/xwayland-satellite/package.nix
@@ -8,7 +8,7 @@ xwayland-satellite.overrideAttrs (old: {
     ++ [
       (fetchpatch {
         url = "https://github.com/Supreeeme/xwayland-satellite/commit/add2795134593faafce60e404a0a75df68e9ee0c.patch";
-        hash = "sha256-XD93f8m8h0o0Vs3QcmWkHGGi5mZwf9wkx9qEiq6sjnw=";
+        hash = "sha256-/1zJYAIHC+xiVytHH5HDt83lZKLBGQQdAoS/y2ObTLc=";
       })
     ];
 })
