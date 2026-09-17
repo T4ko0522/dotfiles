4be119008b4fe0effc1465f021945ea9152e7ae8
T4ko0522 <tako.renraku1@gmail.com>
2026-09-17 12:07:31 +0900

    ✨ upstreamにpatchをあててsceneを描画するように変更

--- nix-configs/pkgs/linux-wallpaperengine/package.nix
+++ nix-configs/pkgs/linux-wallpaperengine/package.nix
@@ -1,4 +1,32 @@
-{linux-wallpaperengine}:
+{
+  dbus,
+  fetchFromGitHub,
+  freetype,
+  linux-wallpaperengine,
+}:
 linux-wallpaperengine.overrideAttrs (old: {
-  patches = (old.patches or []) ++ [./capture-before-fullscreen-pause.patch];
+  version = "0-unstable-2026-07-08";
+
+  src = fetchFromGitHub {
+    owner = "Almamu";
+    repo = "linux-wallpaperengine";
+    rev = "b016d7d1fdcf4e5fd2f9c9fa420a8aaa07fee02d";
+    hash = "sha256-ExWAYdSFW5plPuS3/jxTPMXIly6zVb5GojE3e37imZM=";
+    fetchSubmodules = true;
+  };
+
+  buildInputs =
+    (old.buildInputs or [])
+    ++ [
+      dbus
+      freetype
+    ];
+
+  patches =
+    (old.patches or [])
+    ++ [
+      ./capture-before-fullscreen-pause.patch
+      ./puppet-warp-skeletal-animation.patch
+      ./preserve-authored-image-size.patch
+    ];
 })
